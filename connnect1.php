ab3c048e4e61b2ba7b1f587dd77a6a87e228b4f0
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-11-18 19:29:46 +0900

    feat：ローカルデータベースにアクセスする機能を追加

---
+++ connnect1.php
@@ -0,0 +1,11 @@
+<?php
+$user="phpuser";
+$password="saito1011";
+$opt=[
+  PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES=>false,
+    PDO::MYSQL_ATTR_MULTI_STATEMENTS=>false,
+];
+
+$dbh=new PDO("mysql:host=localhost;dbname=sample_db",$user,$password,$opt);
+var_dump($dbh);
