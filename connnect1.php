597a8ef001a98e8464017d5298fe7b2894df2a84
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-11-18 20:13:59 +0900

    feat：例外処理を追加

--- connnect1.php
+++ connnect1.php
@@ -1,11 +1,22 @@
 <?php
-$user="phpuser";
-$password="saito1011";
-$opt=[
-  PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES=>false,
-    PDO::MYSQL_ATTR_MULTI_STATEMENTS=>false,
-];
 
-$dbh=new PDO("mysql:host=localhost;dbname=sample_db",$user,$password,$opt);
-var_dump($dbh);
+try {
+    $user = "phpuser";
+    $password = "saito1011";
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+    ];
+
+//PDO出の接続
+    $dbh = new PDO("mysql:host=localhost;dbname=sample_db", $user, $password, $opt);
+    var_dump($dbh);
+}catch (PDOException $ex)
+{
+    //->でオブジェクトにアクセスできる
+    echo "エラー！：". $ex->getMessage();
+    exit;
+} finally {
+
+}
